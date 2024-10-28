a26f287604ea77639b5c311e69e572c51a94d6f8
hao <haolata1@gmail.com>
2024-10-28 10:23:18 +0700

    sua loi

--- index.php
+++ index.php
@@ -44,22 +44,9 @@ $result = mysqli_query($conn, $sql);
             // Hiển thị sản phẩm nổi bật mặc định từ truy vấn ban đầu
             while ($row = mysqli_fetch_assoc($result)):
                 ?>
-                <div class="col-lg-3 col-md-4 col-sm-6 mix">
-                    <div class="featured__item">
-                        <div class="featured__item__pic"
-                            style="background-image: url('img/product/<?php echo htmlspecialchars($row['product_image']); ?>');">
-                            <ul class="featured__item__pic__hover">
-                                <li><a href="#"><i class="fa fa-heart"></i></a></li>
-                                <li><a href="#"><i class="fa fa-retweet"></i></a></li>
-                                <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
-                            </ul>
-                        </div>
-                        <div class="featured__item__text">
-                            <h6><a href="#"><?php echo htmlspecialchars($row['product_name']); ?></a></h6>
-                            <h5><?php echo '$' . number_format($row['price'], 2); ?></h5>
-                        </div>
-                    </div>
-                </div>
+               <?php 
+               echo $content;
+               ?>
             <?php endwhile; ?>
         </div>
     </div>
